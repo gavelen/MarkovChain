c9ebf35b3b1b8c3671c5509c4362a417f3396f6d
gavelen <gavelen@gmail.com>
2021-04-19 00:54:48 +0500

    3) пары

--- src/markovChain.java
+++ src/markovChain.java
@@ -11,6 +11,8 @@ public class markovChain {
     public static void main (String[] args) throws IOException {
         String fileName = "/Users/gavelen/Работа/MarkovChain/src/text";
         String text = readFile(fileName);
+       // String[] words = text.split(" ");
+
         System.out.println(text);
 
         List<String> words = new ArrayList<>();
@@ -20,16 +22,33 @@ public class markovChain {
 
         Map<String, Long> frequency = countWords(words);
         System.out.println(frequency);
+
+        System.out.println(pairs(words));
+
+    }
+
+    private static String readFile(String fileName) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(fileName)));
     }
 
     public static Map<String, Long> countWords(List<String> inputList) {
         return inputList.stream().collect(Collectors.toMap(Function.identity(), v -> 1L, Long::sum));
     }
+    public static List<String> pairs (List<String> words) {
+        List<String> pairs = new ArrayList<>();
+        pairs.add("НАЧАЛО" + " " + words.get(0));
+        for (int i = 0; i < words.size() - 1; ++i) {
+            pairs.add(words.get(i) + " " + words.get(i + 1));
 
+        }
+        pairs.add(words.get(words.size()-1) + " " + "КОНЕЦ");
 
-    private static String readFile(String fileName) throws IOException {
-        return new String(Files.readAllBytes(Paths.get(fileName)));
+        return pairs;
     }
+
+
+
+
 
 
 }
