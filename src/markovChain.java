74d7908d7bc89772d4f0f718258bdfdbfe90e479
gavelen <gavelen@gmail.com>
2021-04-19 00:38:01 +0500

    1) прочитала текст 2) посчитала слова

--- src/markovChain.java
+++ src/markovChain.java
@@ -1,15 +1,36 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class markovChain {
     public static void main (String[] args) throws IOException {
         String fileName = "/Users/gavelen/Работа/MarkovChain/src/text";
-        System.out.println(readUsingFiles(fileName));
+        String text = readFile(fileName);
+        System.out.println(text);
+
+        List<String> words = new ArrayList<>();
+        for (String s : text.split(" ")) {
+            words.add(s.replaceAll("[^a-zA-Zа-яёА-ЯЁ]", "").toLowerCase());
+        }
+
+        Map<String, Long> frequency = countWords(words);
+        System.out.println(frequency);
     }
-    private static String readUsingFiles(String fileName) throws IOException {
+
+    public static Map<String, Long> countWords(List<String> inputList) {
+        return inputList.stream().collect(Collectors.toMap(Function.identity(), v -> 1L, Long::sum));
+    }
+
+
+    private static String readFile(String fileName) throws IOException {
         return new String(Files.readAllBytes(Paths.get(fileName)));
     }
+
 
 }
 
